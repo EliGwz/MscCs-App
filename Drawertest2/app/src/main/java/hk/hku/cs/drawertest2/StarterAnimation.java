d6b5057b691fb12d87026b2283caf44cd2446a3c
Eli Gao <eligaowz@hku.hk>
2018-11-08 21:27:18 +0800

    Starter Animation

---
+++ Drawertest2/app/src/main/java/hk/hku/cs/drawertest2/StarterAnimation.java
@@ -0,0 +1,62 @@
+package hk.hku.cs.drawertest2;
+
+import android.content.Intent;
+import android.graphics.drawable.AnimatedImageDrawable;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Window;
+import android.view.WindowManager;
+import android.view.animation.Animation;
+import android.view.animation.ScaleAnimation;
+import android.widget.ImageView;
+
+public class StarterAnimation extends AppCompatActivity {
+    ImageView iv_start;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        supportRequestWindowFeature(Window.FEATURE_NO_TITLE);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
+        setContentView(R.layout.activity_starter_animation);
+        initImage();
+    }
+
+    private void initImage() {
+        iv_start = (ImageView)findViewById(R.id.starterImage);
+        iv_start.setImageResource(R.drawable.brochure);
+
+        ScaleAnimation scaleAnimation = new ScaleAnimation(1.4f, 1.0f, 1.4f,
+                1.0f, Animation.RELATIVE_TO_SELF, 0.5f, Animation.RELATIVE_TO_SELF, 0.5f);
+        scaleAnimation.setDuration(4000);
+
+        scaleAnimation.setFillAfter(true);
+        scaleAnimation.setAnimationListener(new Animation.AnimationListener() {
+            @Override
+            public void onAnimationStart(Animation animation) {
+
+            }
+
+            @Override
+            public void onAnimationEnd(Animation animation) {
+                startActivity();
+            }
+
+            @Override
+            public void onAnimationRepeat(Animation animation) {
+
+            }
+        });
+        iv_start.startAnimation(scaleAnimation);
+    }
+
+    private void startActivity() {
+        Intent switchToMain = new Intent(StarterAnimation.this, MainActivity.class);
+        startActivity(switchToMain);
+        overridePendingTransition(android.R.anim.fade_in, android.R.anim.fade_out);
+        finish();
+    }
+}
