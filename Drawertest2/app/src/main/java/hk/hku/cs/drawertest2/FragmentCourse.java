4035202e237fac33617f3b7e7dce0b6509145d4a
taylorliang <liangmanping@outlook.com>
2018-12-02 20:49:42 +0800

    update course list

--- Drawertest2/app/src/main/java/hk/hku/cs/drawertest2/FragmentCourse.java
+++ Drawertest2/app/src/main/java/hk/hku/cs/drawertest2/FragmentCourse.java
@@ -8,6 +8,8 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ListView;
+import android.widget.SimpleAdapter;
 import android.widget.TextView;
 
 import org.jsoup.Jsoup;
@@ -16,17 +18,23 @@ import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
 
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.HashMap;
 
 public class FragmentCourse extends Fragment {
 
     private TextView tp1;
+    private ListView courseListView, courseListView2;
 
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_course,container,false);
+
         String stringUrl = "https://www.msc-cs.hku.hk/Curriculum/Courses";
-        tp1 = view.findViewById(R.id.course_p11);
+        courseListView = view.findViewById(R.id.courseListView);
+        //courseListView2 = view.findViewById(R.id.courseListView2);
+        //tp1 = view.findViewById(R.id.course_p11);
         new DownloadWebpageTask().execute(stringUrl);
 
         return view;
@@ -46,10 +54,47 @@ public class FragmentCourse extends Fragment {
         }
         @Override
         protected void onPostExecute(Document doc){
+            /***
             Elements courseElements = doc.getElementsByClass("orchard-layouts-root");
             Elements textStringElements = courseElements.select("p");
             String textString = textStringElements.select("p").get(0).text();
             tp1.setText(textString);
+             ***/
+
+            String[] strings = {"course_no", "course_name"}; // map key
+            int[] ids = {R.id.course_number_text, R.id.course_name_text}; // teacher_list.xml
+
+            ArrayList<HashMap<String, Object>> list = new ArrayList<HashMap<String, Object>>();
+            HashMap<String, Object> map = null;
+
+            Elements couseListElements = doc.getElementsByClass("z-depth-3");
+            //Elements eachLineElements = couseListElements.select("td.purple");
+            Elements eachLineElements = couseListElements.select("tbody tr");
+            System.out.println("eachLineElements.size() = " + eachLineElements.size());
+            // cyber security
+            /*for (Element e:  eachLineElements){
+                //map = new HashMap<String, Object>();
+                System.out.println(e.select("p").text());
+            }*/
+
+            map = new HashMap<String, Object>();
+            map.put("course_no", eachLineElements.get(1).select("td").select("p").get(1).text());
+            map.put("course_name", eachLineElements.get(1).select("td").select("p").get(2).text());
+            list.add(map);
+
+            for (int i=2; i<7;i++){
+                map = new HashMap<String, Object>();
+                map.put("course_no", eachLineElements.get(i).select("td").select("p").get(0).text());
+                map.put("course_name", eachLineElements.get(i).select("td").select("p").get(1).text());
+                list.add(map);
+                System.out.println(eachLineElements.get(i).select("td").text());
+            }
+
+            SimpleAdapter simpleAdapter = new SimpleAdapter(getContext(), list, R.layout.course_list, strings, ids);
+            courseListView.setAdapter(simpleAdapter);
+
+
+
         }
 
     }
